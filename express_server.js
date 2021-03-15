da9ac46a2ffe8852f1810d1da7f3fdf11ca19c81
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-15 23:13:19 +0000

    Add redirect to long URL

--- express_server.js
+++ express_server.js
@@ -50,6 +50,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`urls/${randomKey}`);         
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.listen(PORT, () => {
 
   console.log(`Example app listening on port ${PORT}!`);
