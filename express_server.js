38881d1d34dba7867cc357effb388d8c97e6d93a
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-15 21:48:46 +0000

    Add Random alphanumeric characters for short URL

--- express_server.js
+++ express_server.js
@@ -38,6 +38,17 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+function generateRandomString() {
+  const shortID = Math.random().toString(36).substring(2, 8);
+
+  return shortID;
+}
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
