99ea62acf2c260d67fe828eee9413f500d8f8f2c
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-15 18:44:12 +0000

    Set ejs as the view engine.

--- express_server.js
+++ express_server.js
@@ -20,14 +20,10 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+ app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
