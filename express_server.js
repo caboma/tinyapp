e20cb4c83326efe2f57dccd85f2fceee2feeec49
Omar Cabatbat <omar_cabatbat@yahoo.com>
2021-03-15 21:20:36 +0000

    Add Create New URL

--- express_server.js
+++ express_server.js
@@ -21,14 +21,18 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
- app.get("/urls", (req, res) => {
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };;
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  console.log(req.params.longURL)
   res.render("urls_show", templateVars);
 });
 
